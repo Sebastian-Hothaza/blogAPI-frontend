56f8e3b427ca5f0b823325de1a23fe47d91779dd
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-22 22:34:32 -0500

    Added API fetching to show blog posts on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-import './App.css'
 import { Outlet } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <>
-      <div>Hello from App.jsx</div>
-      <Outlet />
-    </>
-  )
+
+  const [error, setError] = useState(null);
+  const [allBlogposts, setAllBlogposts] = useState('');
+
+  async function fetchAPIData(){
+	try{
+	  const response = await fetch('https://hothaza-blogapi.fly.dev/posts');
+	  if (!response.ok) throw new Error("Failed to get API Data");
+	  const data = await response.json();
+	  setAllBlogposts(data);
+	}catch(err){
+	  setError(err.message)
+	}
+  }
+
+  useEffect(() => {
+	fetchAPIData();
+  }, [])
+
+  return (error)?  <div>Error: Could not load data from API!🥹</div> : <Outlet context={allBlogposts}/>
 }
 
 export default App
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,25 @@
+import { useOutletContext } from "react-router-dom"
+import { useEffect, useState } from 'react'
+
 function Home() {
+  const allBlogposts =  useOutletContext();
+  if (!allBlogposts) return <div>...API LOADING...</div>
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div name="blogpostCards">
+
+      
+      {allBlogposts.map(post=>{
+        return (
+          <div name="blogpostCard" key={post._id}>
+            <div name="header" >{post.title}</div>
+            <div name="content">{post.content}</div>
+          </div>
+          
+        )
+      })}
+      
+      
     </div>
   )
 }
