c6043393d0fdadf0514d3c8706539328245cb7c2
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-23 20:23:45 -0500

    Added comment posting ability

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import { useOutletContext } from "react-router-dom"
+import { json, useOutletContext } from "react-router-dom"
 import { useState } from 'react'
 
 function Home() {
@@ -6,11 +6,14 @@ function Home() {
 
 	const [expandedPosts, setExpandedPosts] = useState([]); // Array that tracks which posts we want to have as expanded
 	const [allComments, setAllComments] = useState([]); // Array that tracks which posts we want to have as expanded
+	const [errorMessage, setErrorMessage] = useState(''); //Tracks error message generated when posting a comment
+
 
   	if (!allBlogposts) return <div>...API LOADING...</div>
 
 	// Returns true of child is a subset of parent
 	function checkSubset(parent, child){
+		// This is disgusting
 		// Check each child object
 		nextChild:
 		for (let i = 0; i<child.length; i++){
@@ -39,31 +42,68 @@ function Home() {
 		}
 	}
 
+	// Submits POST request using formData for a specified post.
+	async function handleCommentSubmit(formData, post){
+		formData.preventDefault(); // Stop page from refreshing
+	
+		const response = await fetch('http://localhost:3000/posts/'+post._id+'/comments', {
+			method: "POST",
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+			},
+			body: JSON.stringify({name: formData.target.name.value, comment: formData.target.comment.value})
+		});
+		const data = await response.json();
+		if (!response.ok){
+			setErrorMessage({postID: post._id, msg: 'COMMENT POST FAILED: '+ Object.values(data)[0].msg}); //Prints message of first error object
+			return;
+		}
+		// Append freshly created comment
+		setAllComments(allComments.concat({_id: data._id, parentPost: post._id, name: formData.target.name.value,  comment: formData.target.comment.value}))
+		// Clear form fields and error
+		formData.target.name.value = '';
+		formData.target.comment.value = '';
+		setErrorMessage('');
+	}
+
 	return (
-	<div className="blogpostCards">
-		{allBlogposts.map(post=>{
-		return (
-			<div className="blogpostCard" key={post._id}>
-				<div className="header" >
-					<div>{post.title}</div>
-					<button onClick={() => toggleDetails(post)}>Toggle Details</button>
-				</div>
-				
-				{/* PRINT THE CONTENT */}
-				{expandedPosts.find((p) => p._id === post._id) && 
-					<div className="content">{post.content}</div>
-				}
+		<>
+		<h1>Welcome to my blog</h1>
+		<div className="blogpostCards">
+			{allBlogposts.map(post=>{
+				return (
+					<div className="blogpostCard" key={post._id}>
+						<div className="header" >
+							<div>{post.title}</div>
+							<button onClick={() => toggleDetails(post)}>Toggle Details</button>
+						</div>
+						
+						{/* PRINT THE CONTENTS WHEN EXPANDED */}
+						{expandedPosts.find((p) => p._id === post._id) && 
+							<>
+								<div className="content">{post.content}</div>
+								{/* COMMENTS */}
+								{allComments.filter(comment => comment.parentPost === post._id).map(comment => <div key={comment._id}>{comment.name}: {comment.comment}</div>)}
 
-				{/* PRINT THE CONTENT */}
-				{expandedPosts.find((p) => p._id === post._id) && 
-					allComments.filter(comment => comment.parentPost === post._id).map(comment => <div key={comment._id}>{comment.name}</div>)}
+								{/* COMMENT FORM */}
+								<form onSubmit={(formData) => handleCommentSubmit(formData, post)}>
+									<label htmlFor="name">Name</label>
+									<input type="text" id="name" name="name"></input>
+									<label htmlFor="comment">Comment</label>
+									<input type="text" id="comment" name="comment"></input>
+									<button type="submit">SUBMIT</button>
+						 		</form>
 
+								{errorMessage.postID === post._id && <div>{errorMessage.msg}</div>}
 
-			
-			</div>
-		)
-		})}
-	</div>
+							</>
+							
+						}	
+					</div>
+				)
+			})}
+		</div>
+		</>
 	)
 }
 
